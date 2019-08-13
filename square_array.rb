2dc1b05a9c5e7329085992da57fed0538168b4a2
Eric Loudon <loudon.eric@gmail.com>
2019-08-13 14:28:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
-  squared = []
-  count = 0
-  while count < array.size do
-    squared << (array[count] **2)
-    count = count +1
+  numbers = []
+  array.each do  |number|
+    numbers << (numbers ** 2)
   end
-  squared
+  numbers
 end
